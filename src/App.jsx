be1576baceff7f8547dd056004c643488789bb87
kishanpandya405 <kishanpandya405@gmail.com>
2024-12-06 04:43:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Main from './main/Main'
+import Sidebar from './sidebar/Sidebar'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Sidebar/>
+       
+      </div>
+      <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ src/config/gemini.js
@@ -0,0 +1,36 @@
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+
+  const genAI = new GoogleGenerativeAI("AIzaSyBj9v9O16uxDyhvBu3tBPfTd8e29059oog");
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response = result.response
+    console.log(response.text());
+    return response.text();
+  }
+  
+ export default run;
